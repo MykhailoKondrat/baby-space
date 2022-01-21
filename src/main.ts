beab724354b2544b97f558b81b67953a57cdf889
MykhailoKondrat <mykhailo.kondrat@gmail.com>
2022-01-21 17:02:08 +0200

    basic auth done

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,11 @@ async function bootstrap() {
 bootstrap();
 
 // todo
-// 1.Connect mongo +
-// 1.2 add config .env +
 // 2. add CRUD on post entity + -
-// 3. add search by query params @Query +
-// 3.1 add search by partial query parms
-// 3. add create account (try passport??)
+// 10. add hashing with bcrypt to auth
+// add create account endpoint
+
 // 4. add login
-// 5. add guards for add post routea
 // 6. add refresh token
 // 7. connect users and posts
 // 8. add logger for all requests and responses
